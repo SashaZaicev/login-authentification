69f3dded89f1914d56033389cabcf67f55443c60
Sasha Zaitsau <46328412+SashaZaicev@users.noreply.github.com>
2021-07-12 20:10:23 +0300

    Updates

--- src/selectors.ts
+++
@@ -1,4 +0,0 @@
-import {AppStateType} from './redux/store';
-
-export const selectorRequestStatus= (state: AppStateType) => state.appState.status
-export const selectorAppError= (state: AppStateType) => state.appState.error
